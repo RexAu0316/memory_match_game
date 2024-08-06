c23b93eda720f178519736612c7d50fbde06e14d
RexAu0316 <160446866+RexAu0316@users.noreply.github.com>
2024-08-06 15:14:56 +0800

    Update memory_match_game.js

--- src/memory_match_game.js
+++ src/memory_match_game.js
@@ -7,7 +7,7 @@ window.initGame = (React, assetsUrl) => {
     const [matchedCards, setMatchedCards] = useState([]);
 
     useEffect(() => {
-      const cardImages = ['mario.png', 'Luigi.png', 'Toad.png', 'Yoshi.png'];
+      const cardImages = ['image1.png', 'image2.png', 'image3.png', 'image4.png', 'image5.png', 'image6.png'];
       const shuffledCards = [...cardImages, ...cardImages]
         .sort(() => Math.random() - 0.5)
         .map(image => ({ image, flipped: false }));
